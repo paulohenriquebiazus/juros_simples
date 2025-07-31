f76f4acdb824893db67b730845e78c56169cd326
paulohenriquebiazus <paulo.becker@escola.pr.gov.br>
2025-07-31 08:47:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function calcular(){
+
+    let c = parsefloat(document.getElementById('c').value);
+    let i = parsefloat(document.getElementById('i').value);
+    let t = parsefloat(document.getElementById('t').value);
+    let j = parsefloat(document.getElementById('j').value);
+    let m = parsefloat(document.getElementById('m').value);
+
+    let j = c * i * t;
+    
+    let m = j + c;
+
+}
